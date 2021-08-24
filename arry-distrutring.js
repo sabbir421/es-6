939147d3600fb3345004f07c0b340f64598ac1dd
sabbir421 <sabbiralazim421@gmail.com>
2021-08-25 01:26:28 +0600

    object distructureing

--- arry-distrutring.js
+++ arry-distrutring.js
@@ -1,2 +1,1 @@
-const addition = (num1, num2) =>num1+ num2
-console.log(addition(10,10));
+
---
+++ object-distructureing.js
@@ -0,0 +1,7 @@
+const car = {
+    brand:"bmw",
+    color:"black",
+    price:"$20k"
+}
+const {price,color,brand}=car
+console.log(brand);
